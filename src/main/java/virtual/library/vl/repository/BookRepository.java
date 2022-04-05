f95fc07f3b81a557cbfe050b9e18dfbfe0002858
iesub <70565881+iesub@users.noreply.github.com>
2022-04-05 19:58:54 +0300

    Update BookRepository.java

--- src/main/java/virtual/library/vl/repository/BookRepository.java
+++ src/main/java/virtual/library/vl/repository/BookRepository.java
@@ -11,6 +11,6 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     Book findBookByName(String name);
     @Query(value = "select id, description, logo, name, release_date, author_id, book_genre_id from books order by id desc limit 20 offset ?1",nativeQuery = true)
     List<Book> findBooksWithOffset(Long offset);
-    @Query(value = "select * from books where contains (name, ?1) order by id desc limit 20 offset ?2", nativeQuery = true)
+    @Query(value = "select * from books where contain (name, ?1) order by id desc limit 20 offset ?2", nativeQuery = true)
     List<Book> findBooksByNameContainingIgnoreCase(String name, Long offset);
 }
